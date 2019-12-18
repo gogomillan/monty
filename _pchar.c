a642eee9808dc7da81fcbff9c79175b1c6582eb2
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-12-18 09:08:37 +0000

    Function opcode pchar

--- _pchar.c
+++ _pchar.c
@@ -8,9 +8,15 @@
  */
 void pchar(stack_t **stack, unsigned int line_number)
 {
-	(void) line_number;
-	if (*stack)
+	if (*stack == NULL)
 	{
-		printf("%d\n", (*stack)->n);
+		fprintf(stderr, "L<%d>: can't pchar, stack empty\n", line_number);
+		exit (EXIT_FAILURE);
 	}
+	if ((*stack)->n < 0 || (*stack)->n > 255)
+	{
+		fprintf(stderr, "L<%d>: can't pchar, value out of range\n", line_number);
+		exit (EXIT_FAILURE);
+	}
+	printf("%c\n", (*stack)->n);
 }
