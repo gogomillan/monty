0925dcb0ce64976030fdde5968da746606ac7a64
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-12-18 14:12:16 +0000

    Adjust range of ASCII values

--- _pchar.c
+++ _pchar.c
@@ -11,10 +11,9 @@ void pchar(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
-		freestack(*stack);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)->n < 0 || (*stack)->n > 127)
+	if ((*stack)->n < 32 || (*stack)->n > 127)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 		freestack(*stack);
