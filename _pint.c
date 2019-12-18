d71e2c8d26c4a2865bff534d73f4d8c3d727ef3b
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-12-18 14:01:18 +0000

    Error msg to stderr in pint opcode function

--- _pint.c
+++ _pint.c
@@ -9,8 +9,10 @@
 void pint(stack_t **stack, unsigned int line_number)
 {
 	(void) line_number;
-	if (*stack)
+	if (*stack == NULL)
 	{
-		printf("%d\n", (*stack)->n);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
 	}
+	printf("%d\n", (*stack)->n);
 }
