46533b962a83ea3cebd8a1c3b0671009a9c750a0
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2019-12-18 03:11:36 +0000

    Refactoring readfile.c

--- readfile.c
+++ readfile.c
@@ -7,44 +7,35 @@
  */
 void readfile(char *file)
 {
-int fd, r, i, cmd_pos = 0, num_pos = 0, cmd_found = 0;
-char *buffer,  command[100],  number[100];
+char *buffer = NULL, *tmp, *token, command[100],  number[100];
+FILE *fp;
+size_t len = 0;
+ssize_t read;
 
-	buffer = malloc(sizeof(char) * BUFFER_SIZE);
-	fd = open(file, O_RDONLY);
-	if (fd < 0)
+	fp = fopen(file, "r");
+	if (fp == NULL)
 	{	printf("Error: Can't open file %s\n", file);
 		exit(EXIT_FAILURE);
 	}
-	while ((r = read(fd, buffer, BUFFER_SIZE)) > 0)
-		for (i = 0; i < r; i++)
-		{
-			if (buffer[i] != ' ' && buffer[i] != '\n')
-				command[cmd_pos] = buffer[i], cmd_pos++, cmd_found = 1;
-			if ((buffer[i] == ' ' || buffer[i] == '\n') && cmd_found == 1)
-			{
-				while (buffer[i] != '\n')
-				{
-					if (buffer[i] >= 48 && buffer[i] <= 57)
-						number[num_pos] = buffer[i], num_pos++;
-					else if (buffer[i] == ' ')
-					{
 
-					}
-					else
-					{
-						while (buffer[i] != '\n')
-							i++;
-						break;
-					}
-					i++;
-				}
-				command[cmd_pos] = '\0', number[num_pos] = '\0';
-				/* check_command(command); */
-				printf("%s\n", command), set_number(number);
-				memset(command, '\0', 100), memset(command, '\0', 100);
-				cmd_pos = 0, num_pos = 0, cmd_found = 0;
-			}
+	while ((read = getline(&buffer, &len, fp)) != -1)
+	{
+		tmp = strdup(buffer);
+		token = strtok(tmp, " \t\n\r");
+		if (strncmp(token, "push", 4) == 0)
+		{
+			strcpy(command, token);
+			token = strtok(NULL, " \n\r\t");
+			strcpy(number, token);
+			printf("%s | %s\n", command, number);
 		}
+		else
+		{
+			strcpy(command, token);
+			printf("%s\n", command);
+		}
+		free(tmp);
+	}
+	fclose(fp);
 	free(buffer);
 }
