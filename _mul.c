60b0388284aacccaab86411671ce4fd2e9b7c1ca
Juan Marcos Cabezas <1225@holbertonschool.com>
2020-01-04 03:29:06 +0000

    Adding mul file

---
+++ _mul.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * mul - sub the two top elements of the stack
+ * @stack: The stack
+ * @line_number: The current file line number
+ * Return: Nothing or EXIT_FAILURE on failure
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+	int substraction = 0;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L<%d>: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->next == NULL)
+	{
+		fprintf(stderr, "L<%d>: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	pop(stack, line_number);
+	substraction = glob_n;
+	pop(stack, line_number);
+	substraction -= glob_n;
+	glob_n = substraction * -1;
+	push(stack, line_number);
+}
