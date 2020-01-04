ccae033252aaff34f959ed974a40e3dc9f596c2b
Juan Marcos Cabezas <1225@holbertonschool.com>
2020-01-04 03:46:07 +0000

    Adding prototype

--- monty.h
+++ monty.h
@@ -53,6 +53,10 @@ void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
+void div(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+
+
 
 
 #endif
